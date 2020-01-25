0c75b040b82da3c7356d13f0a241b4cd1adc2f93
MungaiKeren <wambukeren@gmail.com>
2020-01-25 13:41:41 +0300

    did a simple while loop

---
+++ whileloop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>while loop</title>
+</head>
+<body>
+    <?php 
+    
+    $counter=0;
+    while($counter<10){
+        echo $counter;
+        $counter++;
+    }
+    
+    ?>
+</body>
+</html>
