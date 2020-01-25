ec2f3015fb4587b99f54766a9b36e0e990cebe78
MungaiKeren <wambukeren@gmail.com>
2020-01-25 13:53:26 +0300

    for loop

---
+++ for loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>for-loop</title>
+</head>
+<body>
+    <?php 
+        $counter=1;
+        for($counter; $counter< 10; $counter++){
+            echo $counter."<br>";
+        }
+    ?>
+</body>
+</html>
